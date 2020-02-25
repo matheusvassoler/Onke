adf8d2303804cf253f4d5ebae44fa2ca50905209
Matheus da Silva Vassoler <mvassoler@ciandt.com>
2020-02-25 12:33:40 -0300

    Create the ContactBusiness and actions to buttons in contact form and main form

--- src/ui/ContactForm.java
+++ src/ui/ContactForm.java
@@ -1,6 +1,9 @@
 package ui;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class ContactForm extends JFrame {
 
@@ -14,5 +17,32 @@ public class ContactForm extends JFrame {
         setContentPane(rootPanel);
         setSize(500, 250);
         setVisible(true);
+
+        //Obtem o tamanho da tela
+        //getDefaultToolkit obtem a tela do monitor
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+
+        //Define a posição da janela no monitor
+        setLocation(dim.width/2 - getSize().width / 2, dim.height/2 - getSize().height / 2);
+
+        setListeners();
+    }
+
+    private void setListeners() {
+
+        buttonSave.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        buttonCancel.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new MainForm();
+                dispose();
+            }
+        });
     }
 }
--- src/ui/MainForm.java
+++ src/ui/MainForm.java
@@ -2,6 +2,8 @@ package ui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainForm extends JFrame{
     private JPanel rootPanel;
@@ -22,5 +24,25 @@ public class MainForm extends JFrame{
         setLocation(dim.width/2 - getSize().width / 2, dim.height/2 - getSize().height / 2);
 
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        setListeners();
+    }
+
+    //AddActionListener recebe uma instancia responsavel por executar ação do botão
+    private void setListeners() {
+        buttonNewContact.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new ContactForm();
+                dispose();
+            }
+        });
+
+        buttonRemove.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
     }
 }
