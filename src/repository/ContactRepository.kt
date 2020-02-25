19f59587b3aed9a1e76ae6db0ada0de85dee6d70
Matheus da Silva Vassoler <mvassoler@ciandt.com>
2020-02-25 17:55:23 -0300

    Implements the function to remove a contact

--- src/repository/ContactRepository.kt
+++ src/repository/ContactRepository.kt
@@ -14,7 +14,16 @@ class ContactRepository {
         }
 
         fun delete(contact: ContactEntity) {
+            //contactList.remove(contact)
 
+            var index = 0
+            for(item in contactList.withIndex()) {
+                if(item.value.name == contact.name && item.value.phone == contact.phone) {
+                    index = item.index
+                    break
+                }
+            }
+            contactList.removeAt(index)
         }
 
         //List é uma lista que aceita valores de diversos tipos
